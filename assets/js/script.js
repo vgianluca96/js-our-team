046e35f65ae9ad6cc2d7216048cedf71a4f7e378
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-06 14:36:42 +0200

    milestone 1
    stampate, per ogni membro del team, le informazioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,39 @@
 
 
+// Creo array di oggetti con i dati dei membri del team
 const teamMembers = [
 
-    teamMember1 = {
+    {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
         img: 'wayne-barnett-founder-ceo.jpg'
     },
 
-    teamMember2 = {
+    {
         name: 'Angela Caroll',
         role: 'Chief Editor',
         img: 'angela-caroll-chief-editor.jpg'
     },
 
-    teamMember3 = {
+    {
         name: 'Walter Gordon',
         role: 'Office Manager',
         img: 'walter-gordon-office-manager.jpg'
     },
 
-    teamMember4 = {
+    {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
         img: 'angela-lopez-social-media-manager.jpg'
     },
 
-    teamMember5 = {
+    {
         name: 'Scott Estrada',
         role: 'Developer',
         img: 'scott-estrada-developer.jpg'
     },
 
-    teamMember6 = {
+    {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
         img: 'barbara-ramos-graphic-designer.jpg'
@@ -40,6 +41,16 @@ const teamMembers = [
 
 ];
 
-console.log(teamMembers);
-console.log(teamMembers[0]);
-console.log(teamMembers[0].name);
+
+// Stampo in console ogni oggetto e i dati conenuti in esso
+for (let i = 0; i < teamMembers.length; i++) {
+
+    console.log(teamMembers[i]);
+
+    for (let key in teamMembers[i]) {
+        console.log(teamMembers[i][key]);
+    }
+
+}
+
+
