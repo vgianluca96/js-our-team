77ba531758e8c8ad5ae19202b791c7d577055aff
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-06 14:28:10 +0200

    milestone 0
    creato array di oggetti

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,45 @@
 
-console.log('ciao');
+
+const teamMembers = [
+
+    teamMember1 = {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    teamMember2 = {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+
+    teamMember3 = {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+
+    teamMember4 = {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+
+    teamMember5 = {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+
+    teamMember6 = {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    },
+
+];
+
+console.log(teamMembers);
+console.log(teamMembers[0]);
+console.log(teamMembers[0].name);
